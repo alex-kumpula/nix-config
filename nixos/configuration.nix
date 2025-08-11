7a916f4c08f574985e856606f0e6a9ef28469603
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-11 15:58:27 -0600

    Remove duplicate hostName

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -78,7 +78,6 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
