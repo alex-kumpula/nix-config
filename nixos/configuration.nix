5a6655184448030cfd7cec25bd67839aa90c876d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-11 16:02:07 -0600

    Disable pulseaudio through hardware config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -111,6 +111,7 @@
 
   # Enable sound with pipewire.
   # services.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
