24838caef68a3cd020eae68539118105067c51f6
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-11 23:40:40 -0600

    swap plasma6 for cinnamon

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -100,7 +100,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
