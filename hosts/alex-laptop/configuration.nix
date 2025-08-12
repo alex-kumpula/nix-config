a717ccd6947e04e51f0e2f7aad104565cd6d8f7d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-11 23:35:38 -0600

    add plasma6

--- hosts/alex-laptop/configuration.nix
+++ hosts/alex-laptop/configuration.nix
@@ -100,6 +100,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
