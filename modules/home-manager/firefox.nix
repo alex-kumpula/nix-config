31ee90bed994eac5ff5841e0db6e4ccc52208de7
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-15 02:24:13 -0600

    test5

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -10,6 +10,16 @@ in
       default = pkgs.firefox;
       description = "The Firefox browser package";
     };
+    options.alexModules.programs.firefox.profiles = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule {
+        options = {
+          name = lib.mkOption { type = lib.types.str; default = "Default"; };
+          extensions = lib.mkOption { type = lib.types.listOf lib.types.str; default = []; };
+        };
+      });
+      default = {};
+      description = "Firefox profiles";
+    };
   };
 
   config = lib.mkIf cfg.programs.firefox.enable {
